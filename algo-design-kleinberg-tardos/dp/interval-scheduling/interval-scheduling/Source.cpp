8313e2c7d98ef265e04b8e191f9c0f9648752041
Vincent Gim <vgim@sateva.com.au>
2017-07-24 22:48:32 +0800

    INTSCHED: initial solution

---
+++ algo-design-kleinberg-tardos/dp/interval-scheduling/interval-scheduling/Source.cpp
@@ -0,0 +1,24 @@
+﻿/*
+ * Explanation:  OPT(j) = max(vj + OPT(p(j)), OPT(j−1))
+ * OPT(j): the optimal solution at index j
+ *
+ * If an optimal solution includes the interval at index j, then:
+ * vj: the value of j, that is, the reward for taking into consideration an interval at index j
+ * OPT(p(j)): the optimal value before the interval at j. This is because if j part of an optimal solution, O, then
+ * we must not have any other intervals in O that intersects with interval j
+ *
+ * Otherwise, the optimal solution when considering the interval at index j is the same as
+ * the optimal solution without considering interval at j.
+ * OPT(j - 1): the optimal solution without interval at j
+ */
+#include<iostream>
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+int main() {
+
+
+	return 1;
+}
