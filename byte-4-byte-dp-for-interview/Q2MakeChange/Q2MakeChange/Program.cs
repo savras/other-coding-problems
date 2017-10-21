10d0309ba8d572be3d6c098f3f86a23b4cdb55f1
Vincent Gim <vgim@sateva.com.au>
2017-10-21 16:17:40 +0800

    Turn solution around & top down

--- byte-4-byte-dp-for-interview/Q2MakeChange/Q2MakeChange/Program.cs
+++ byte-4-byte-dp-for-interview/Q2MakeChange/Q2MakeChange/Program.cs
@@ -25,9 +25,31 @@ namespace Q2MakeChange
             return cache[n];
         }
 
+        public static int MakeChangeTopDown(int n, int[] cache)
+        {
+            var size = n + 1;
+            for (var i = 1; i < size; i++)
+            {
+                if (i%6 == 0)
+                {
+                    cache[i] = cache[i - 6] + 1;
+                }
+                else if (i%10 == 0)
+                {
+                    cache[i] = cache[i - 10] + 1;
+                }
+                else
+                {
+                    cache[i] = cache[i - 1] + 1;
+
+                }
+            }
+            return cache[n];
+        }
+
         static void Main(string[] args)
         {
-            var n = 10;
+            var n = 7;
             var cache = new int[n+1];
 
             // So that cache[0] is 0, the rest are -1, and cache[0] is used as termination condition for recursion.
@@ -36,7 +58,8 @@ namespace Q2MakeChange
                 cache[i] = -1;
             }
 
-            var result = MakeChange(n, cache);
+            //var result = MakeChange(n, cache);
+            var result = MakeChangeTopDown(n, cache);
             Console.WriteLine(result);
         }
     }
