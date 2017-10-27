8b7a5ad9c7665f58a34822ffc83257c3f2fb9b59
Vincent Gim <vgim@sateva.com.au>
2017-10-27 23:24:49 +0800

    ctci: q1 with data structure

--- ctci-practice/chp1-arrays/q1/q1/Program.cs
+++ ctci-practice/chp1-arrays/q1/q1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,33 @@ namespace q1
 {
     class Program
     {
+        static bool IsUnique(string str)
+        {
+            var result = true;
+            var len = str.Length;
+            var countArr = new int[26];
+
+            for (var i = 0; i < len; i++)
+            {
+                if (countArr[str[i] - 'a'] == 0)
+                {
+                    countArr[str[i] - 'a']++;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            var str = Console.ReadLine();
+            var result = IsUnique(str);
+            Console.WriteLine(result);
         }
     }
 }
