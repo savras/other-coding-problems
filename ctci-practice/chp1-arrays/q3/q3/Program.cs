7220caba150509e1bdcf0f895631802aa1382958
Vincent Gim <vgim@sateva.com.au>
2017-10-31 23:59:34 +0800

    ctci: q3 initial logic

--- ctci-practice/chp1-arrays/q3/q3/Program.cs
+++ ctci-practice/chp1-arrays/q3/q3/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace q3
+﻿using System;
+using System.Text;
+
+namespace q3
 {
     class Program
     {
+        static string URLify(string str)
+        {
+            var sb = new StringBuilder(str);
+            var i = 0;
+            while(i < sb.Length)
+            {
+                if (sb[i] != ' ') { i++; continue; }
+
+                int j;
+                for (j = i + 1; j < sb.Length; j++)
+                {
+                    if (sb[j] != ' ') { break; }
+                }
+
+                var substring = sb.ToString().Substring(j);
+                sb.Insert(i, "%20");
+                i += 2;
+                sb.Remove(i, sb.Length);
+                sb.Insert(i, substring);
+
+                i++;
+            }
+            return sb.ToString();
+        }
+
         static void Main(string[] args)
         {
+            var str = Console.ReadLine();
+            Console.WriteLine(URLify(str));
         }
     }
 }
