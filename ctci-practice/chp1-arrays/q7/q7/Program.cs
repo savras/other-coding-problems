8fd72f2d096c75fb4218c89c60913615fc0dabd6
Vincent Gim <vgim@sateva.com.au>
2017-11-05 11:20:26 +0800

    ctci: q7 - fixed logic

--- ctci-practice/chp1-arrays/q7/q7/Program.cs
+++ ctci-practice/chp1-arrays/q7/q7/Program.cs
@@ -1,8 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Initial
+ * { 1, 2, 3, 4},
+ * { 5, 6, 7, 8},
+ * { 9, 10, 11, 12 },
+ * { 13, 14, 15, 16 }
+ * 
+ * Result
+ * { 13, 9, 5, 1},
+ * { 14, 10, 6, 2},
+ * { 15, 11, 7, 3 },
+ * { 16, 12, 8, 4 } 
+ */
+
+using System;
 
 namespace q7
 {
@@ -10,22 +19,32 @@ namespace q7
     {
         static void Main(string[] args)
         {
-            var n = int.Parse(Console.ReadLine());
-            var arr = new int[n, n];
+            var n = 4;
+            var arr = new[, ]
+            {
+                { 1, 2, 3, 4},
+                { 5, 6, 7, 8},
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+            };
+
 
             // if n %2 != 0, outer loop loops from i = 0 to i = n/2 (incl)
             // if n %2 == 0, outer loop loops from i = 0 to < n/2 -1
-            var limit = n % 2 == 0 ? (n / 2) - 1 : (n / 2);
+            var limit = n / 2;
 
             for (var i = 0; i < limit; i++)
             {
-                for (var j = 0; j < n; j++)
+                var maxIndex = n - i - 1;
+                for (var j = i; j < maxIndex; j++)
                 {
+                    var descendingIndex = maxIndex - j + i;
+
                     var temp = arr[i,j];
-                    arr[i,j] = arr[n - j,i];
-                    arr[n - j,i] = arr[n,n - j];
-                    arr[n,n - j] = arr[j,n];
-                    arr[j,n] = temp;
+                    arr[i,j] = arr[descendingIndex, i];
+                    arr[descendingIndex, i] = arr[maxIndex, descendingIndex];
+                    arr[maxIndex, descendingIndex] = arr[j,maxIndex];
+                    arr[j,maxIndex] = temp;
                 }
             }
         }
