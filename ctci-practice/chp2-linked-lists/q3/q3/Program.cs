fd2be0bd667cc178336afdd5055d4c2672a9a42e
Vincent Gim <vgim@sateva.com.au>
2017-11-07 22:25:04 +0800

    ctci: chp2 - q3

---
+++ ctci-practice/chp2-linked-lists/q3/q3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace q3
+{
+    class Node()
+    {
+        
+    }
+
+    class Program
+    {
+        static void DeleteNode(Node node)   // Will not be given head nor tail.
+        {
+            var current = node;
+            var next = current.Next;
+            var last = null;
+            while (next != null)
+            {
+                if (next.Next == null)
+                {
+                    last = next;
+                }
+                current.Value = next.Value;
+
+                if (last != null)
+                {
+                    current.Next = null
+                }
+                else
+                {
+                    current = next;
+                }
+                next = next.Next;
+            }
+            previous.Next = null;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
