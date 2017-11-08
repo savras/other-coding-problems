791851880009772e4cb368d1bca2de42dbb1b808
Vincent Gim <vgim@sateva.com.au>
2017-11-09 00:09:18 +0800

    ctci: chp2 - q5

---
+++ ctci-practice/chp2-linked-lists/q5/q5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml;
+
+namespace q5
+{
+    class Program
+    {
+        Node Sum(Node head1, Node head2)
+        {
+            var num1 = GetInt(head1);
+            var num2 = GetInt(head2);
+            var sum = num1 + num2;
+
+            var resultHead = BuildLinkedList(sum);
+            return resultHead;
+        }
+
+        Node BuildLinkedList(int num)
+        {
+            var head = new Node();
+            while (num > 0)
+            {
+                var value = num%10;
+                head.AddNewNode(value);
+                value = value/10;
+            }
+            return head;
+        }
+
+        int GetInt(Node head)
+        {
+            var current = head;
+            var multiplier = 1;
+            var runningSum = 0;
+            while (current != null)
+            {
+                runningSum += (current.Value()*multiplier);
+                current = current.Next();
+                multiplier *= 10;
+            }
+            return runningSum;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
