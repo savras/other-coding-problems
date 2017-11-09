e1091926087216f105128223ccefadeaf26e3ceb
Vincent Gim <vgim@sateva.com.au>
2017-11-09 21:46:56 +0800

    ctci: chp2 - q5 - reversed

--- ctci-practice/chp2-linked-lists/q5/q5/Program.cs
+++ ctci-practice/chp2-linked-lists/q5/q5/Program.cs
@@ -14,6 +14,39 @@ namespace q5
             return resultHead;
         }
 
+        Node SumReversed(Node head1, Node head2)
+        {
+            var num1 = GetIntReversed(head1);
+            var num2 = GetIntReversed(head2);
+            var sum = num1 + num2;
+
+            var resultHead = BuildLinkedList(sum);
+            return resultHead;
+        }
+
+        int GetIntReversed(Node head)
+        {
+            var stack = new Stack<int>();
+
+            var current = head;
+            while (current != null)
+            {
+                stack.Push(current.Value());
+                current = current.Next();
+            }
+
+            var result = 0;
+            var multiplier = 1;
+            while (!stack.Empty())
+            {
+                result += multiplier*stack.Top();
+                stack.Pop();
+                multiplier *= 10;
+            }
+
+            return result;
+        }
+
         Node BuildLinkedList(int num)
         {
             var head = new Node();
@@ -42,7 +75,7 @@ namespace q5
 
         static void Main(string[] args)
         {
-
+            // Call Sum() & SumReversed();
         }
     }
 }
