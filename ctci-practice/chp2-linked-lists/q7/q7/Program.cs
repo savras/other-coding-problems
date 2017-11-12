dfdbb078c8c368687cef878134ad505df279ff82
Vincent Gim <vgim@sateva.com.au>
2017-11-12 20:04:02 +0800

    ctci: q7 - initial solution for topological sort

---
+++ ctci-practice/chp2-linked-lists/q7/q7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace q7
+{
+    class Program
+    {
+
+        static void PerformTopoSort(List<List<int>> adjList)
+        {
+            var visited = new HashSet<int>();
+            var result = new List<int>();
+            visited.Add(0);
+            PerformDfs(visited, result, adjList, 0);
+        }
+
+        static void PerformDfs(HashSet<int> visited, List<int> result, List<List<int>> adjList, int nodeIndex)
+        {
+            foreach (var neighbour in adjList[nodeIndex])
+            {
+                if (!visited.Contains(neighbour))
+                {
+                    visited.Add(neighbour);
+                    PerformDfs(visited, result, adjList, neighbour);
+                }
+            }
+        }
+
+        // Topological sort
+        static void Main(string[] args)
+        {
+            var adjList = new List<List<int>>
+            {
+                new List<int> { 1, 2, 4 },
+                new List<int> {3, 4, 6 }
+            };
+
+            PerformTopoSort(adjList);
+        }
+    }
+}
