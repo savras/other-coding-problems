291f28423874fd3434b6b8ddbb275c0988689dfc
Vincent Gim <vgim@sateva.com.au>
2017-11-15 23:33:45 +0800

    ctci: chp8 - q1 brute force

--- ctci-practice/chp8-dp/q1/q1/Program.cs
+++ ctci-practice/chp8-dp/q1/q1/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace q1
 {
     class Program
     {
+        static int GetSteps(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            
+            var result = 0;
+            for (var i = 1; i <= 3; i++)
+            {
+                if (i <= n)
+                {
+                    result += GetSteps(n - i);
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            var result = GetSteps(n);
+            Console.WriteLine(result);
         }
     }
 }
