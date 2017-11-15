09d85783ad2b6223e861400ba12fa69c1d2f0df4
Vincent Gim <vgim@sateva.com.au>
2017-11-16 00:07:47 +0800

    ctci: chp 8 - memo solution

--- ctci-practice/chp8-dp/q1/q1/Program.cs
+++ ctci-practice/chp8-dp/q1/q1/Program.cs
@@ -1,31 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace q1
 {
     class Program
     {
-        static int GetSteps(int n)
+        static int GetStepsMemoize(int n, Dictionary<int, int> cache)
         {
             if (n <= 0)
             {
                 return 1;
             }
-            
+
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+
             var result = 0;
             for (var i = 1; i <= 3; i++)
             {
                 if (i <= n)
                 {
-                    result += GetSteps(n - i);
+                    result += GetStepsMemoize(n - i, cache);
+                }
+            }
+            cache.Add(n, result);
+
+            return result;
+        }
+
+        static int GetStepsBruteforce(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+
+            var result = 0;
+            for (var i = 1; i <= 3; i++)
+            {
+                if (i <= n)
+                {
+                    result += GetStepsBruteforce(n - i);
                 }
             }
             return result;
         }
+
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
-            var result = GetSteps(n);
-            Console.WriteLine(result);
+            if (n == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+
+                //var result = GetStepsBruteforce(n);
+
+                var cache = new Dictionary<int, int>();
+                var result = GetStepsMemoize(n, cache);
+
+                //var result = GetStepsDp(n);
+                Console.WriteLine(result);
+            }
         }
     }
 }
