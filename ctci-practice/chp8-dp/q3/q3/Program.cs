4fb0b3cd88fdd15322e280f3f094834f9f4d4bce
Vincent Gim <vgim@sateva.com.au>
2017-11-21 23:19:48 +0800

    ctci: chp8 - binary search for distinct n

---
+++ ctci-practice/chp8-dp/q3/q3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] {-40, -20, 0, -2, 5, 5, 29};
+            var len = numbers.Length;
+            var result = FindMagicNumber(numbers, 0, len);
+            Console.WriteLine(result);
+        }
+
+        private static int FindMagicNumber(int[] numbers, int low, int high)
+        {
+            if (high == low)
+            {
+                return -1;
+            }
+
+            var mid = low + (high - low)/2;
+            if (numbers[mid] == mid)
+            {
+                return mid;
+            }
+
+            if (numbers[mid] < mid)
+            {
+                return FindMagicNumber(numbers, mid + 1, high);
+            }
+            return FindMagicNumber(numbers, 0, mid - 1); 
+        }
+    }
+}
