2b85366f15364f2c6059f59c06fff628170ebfa4
Vincent Gim <vgim@sateva.com.au>
2017-11-26 19:04:44 +0800

    ctci: chp8 - q11 added memoized solution

--- ctci-practice/chp8-dp/q11/q11/Program.cs
+++ ctci-practice/chp8-dp/q11/q11/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace q11
 {
     class Program
     {
+        static readonly List<int> Denominations = new List<int> { 25, 10, 5 };
+
+        static int GetNumberOfWaysToRepresentNMemo(int n, Dictionary<int, int> cache)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n < 0)
+            {
+                return 0;
+
+            }
+
+            var result = 0;
+
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+
+            foreach (var denom in Denominations)
+            {
+                result += GetNumberOfWaysToRepresentNMemo(n - denom, cache);
+            }
+            cache.Add(n, result);
+            return cache[n];
+        }
+
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            if (n % 5 != 0)
+            {
+                Console.WriteLine("Cannot fully represent n with the denominations");
+            }
+            else
+            {
+                var cache = new Dictionary<int, int>();
+                var result = GetNumberOfWaysToRepresentNMemo(n, cache);
+                Console.WriteLine(result);
+            }
+
         }
     }
 }
