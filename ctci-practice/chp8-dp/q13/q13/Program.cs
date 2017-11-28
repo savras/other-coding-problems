bb6f579da0b9715df37332bf051d4768a24fd478
Vincent Gim <vgim@sateva.com.au>
2017-11-28 22:50:35 +0800

    ctci: chp8 - q13 commit before changing logic

--- ctci-practice/chp8-dp/q13/q13/Program.cs
+++ ctci-practice/chp8-dp/q13/q13/Program.cs
@@ -5,48 +5,62 @@ namespace q13
 {
     class Program
     {
-        static int GetMaxHeight(List<List<int>> boxes, int currentIndex)
+        static int GetMaxHeight(List<List<int>> boxes, int currentIndex, int lastPickedIndex, List<int> cache)
         {
             if (currentIndex >= boxes.Count)
             {
                 return 0;
             }
 
-            var result = boxes[currentIndex][0];    // ToDo:: Better way to handle currentIndex == 0 
-
-            if (currentIndex > 0 && currentIndex < boxes.Count)
+            if (cache[currentIndex] != 0)
             {
-                result = boxes[currentIndex][0];
+                return cache[currentIndex];
+            }
 
-                if (boxes[currentIndex][0] > boxes[currentIndex - 1][0] &&
-                    boxes[currentIndex][1] > boxes[currentIndex - 1][1] &&
-                    boxes[currentIndex][2] > boxes[currentIndex - 1][2])
+            var result = boxes[currentIndex][0];
+            if (currentIndex == lastPickedIndex)
+            {
+                result += GetMaxHeight(boxes, currentIndex + 1, currentIndex, cache);
+            }
+            else    // currentIndex > 0 && currentIndex < boxes.Count
+            {
+                if (boxes[currentIndex][0] < boxes[lastPickedIndex][0] &&
+                    boxes[currentIndex][1] < boxes[lastPickedIndex][1] &&
+                    boxes[currentIndex][2] < boxes[lastPickedIndex][2])
                 {
-                    result += GetMaxHeight(boxes, currentIndex + 1);
+                    result += GetMaxHeight(boxes, currentIndex + 1, currentIndex, cache);
                 }
                 else
                 {
-                    result += GetMaxHeight(boxes, currentIndex + 2);
+                    result = 0;
+                    result += GetMaxHeight(boxes, currentIndex + 1, lastPickedIndex, cache);
                 }
             }
-            else
-            {
-                result += GetMaxHeight(boxes, currentIndex + 1);
-            }
-            return result;
+
+            cache[currentIndex] = result;
+            return cache[currentIndex];
         }
 
         static void Main(string[] args)
         {
-            var boxes = new List<List<int>> { new List<int>{ 5, 4, 3 } };
+            var boxes = new List<List<int>> { new List<int>{ 5, 5, 5 },
+                                              new List<int>{ 4, 6, 4},
+                                              new List<int>{ 3, 3, 3},
+                                            };
             var orderedBoxes = OrderBoxesByHeight(boxes);
-            var result = GetMaxHeight(orderedBoxes, 0);
-            Console.WriteLine(result);
+            var cache = new List<int>(boxes.Count) { 0, 0, 0 };
+            for(var i = 0; i < boxes.Count; i++)
+            {
+                if (cache[i] == 0)
+                {
+                    GetMaxHeight(orderedBoxes, i, i, cache);
+                }
+            }
         }
 
         static List<List<int>> OrderBoxesByHeight(List<List<int>> boxes)
         {
-            return new List<List<int>>();
+            return boxes ;
         }
     }
 }
