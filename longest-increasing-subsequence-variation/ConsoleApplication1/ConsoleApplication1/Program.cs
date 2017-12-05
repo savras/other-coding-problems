fdf68d43eb6f8e7b285b67561a9467b2d4e0f406
Vincent Gim <vgim@sateva.com.au>
2017-12-05 20:16:11 +0800

    LISV: Completed O(n^2) solution

--- longest-increasing-subsequence-variation/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ longest-increasing-subsequence-variation/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,23 +11,29 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var list = new List<List<int>>
+            // Order by multiplication of base dimensions
+            // Key observation: two factors of a number will always have one 
+            // factor (in the same position e.g. width) larger or smaller than the other.
+            // By ordering them by area we are getting small pieces of valid stacks. 
+            // When we loop over the dp table array we are essentially looping past all smaller, valid box stacks
+            // and looking for the one with the largest height.
+            var orderedList = new List<List<int>>
             {
-                new List<int> {1, 2, 3},
                 new List<int> {3, 1, 2},
                 new List<int> {2, 1, 3},
-                new List<int> {4, 5, 6},
-                new List<int> {4, 6, 7},
+                new List<int> {1, 2, 3},
+                new List<int> {6, 4, 5},
                 new List<int> {5, 4, 6},
                 new List<int> {7, 4, 6},
-                new List<int> {6, 4, 5},
                 new List<int> {6, 4, 7},
-                new List<int> {10, 12, 32},
+                new List<int> {4, 5, 6},
+                new List<int> {4, 6, 7},
+                new List<int> {32, 10, 12},
                 new List<int> {12, 10, 32},
-                new List<int> {32, 10, 12}
+                new List<int> {10, 12, 32}
             };
 
-            var result = GetGreatestHeight(list, list.Count);
+            var result = GetGreatestHeight(orderedList, orderedList.Count);
             Console.WriteLine(result);
         }
 
@@ -36,14 +42,18 @@ namespace ConsoleApplication1
             var cache = new int[n];
             for (var i = 0; i < n; i++)
             {
-                cache[n] = list[i][0];
+                cache[i] = list[i][0];
             }
 
-            for (var i = 0; i < n; i++)
+            for (var i = 1; i < n; i++)
             {
-                for (var j = 0; j < n; j++)
+                for (var j = i - 1; j >= 0; j--)
                 {
-
+                    if (list[j][1] < list[i][1] &&
+                        list[j][2] < list[i][2])
+                    {
+                        cache[i] = Math.Max(cache[i], list[i][0] + cache[j]);
+                    }
                 }
             }
 
