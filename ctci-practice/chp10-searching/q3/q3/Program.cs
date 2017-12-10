403c802ed6245747d2443798443e5a64fa257e7c
Vincent Gim <vgim@sateva.com.au>
2017-12-10 15:49:09 +0800

    ctci: chp10 - q3 solution

--- ctci-practice/chp10-searching/q3/q3/Program.cs
+++ ctci-practice/chp10-searching/q3/q3/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace q3
 {
     class Program
     {
+        static int FindPivot(int[] A, int low, int high)
+        {
+            var mid = low + ((high - low) / 2);
+            var result = mid + 1;   // Start of array is the next index
+
+            if (A[low] > A[mid])
+            {
+                result = Math.Max(FindPivot(A, low, mid), result);
+            }
+            else if (A[mid + 1] > A[high])
+            {
+                result = Math.Max(FindPivot(A, mid + 1, high), result);
+            }
+
+            return result;
+        }
+
+        static int FindElement(int[] A, int e, int low, int high)
+        {
+            var mid = low + (high - low) / 2;
+            var result = -1;
+
+            if (A[mid] == e)
+            {
+                result = mid;
+            }
+            else if (A[mid] != e && high > low)
+            {
+                if (e < A[mid])
+                {
+                    result = Math.Max(FindElement(A, e, low, mid), result);
+                }
+                else
+                {
+                    result = Math.Max(FindElement(A, e, mid + 1, high), result);
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            var A = new[] { 10, 14, 16, 18, 20, 21, 1, 5 };
+            var n = A.Length;
+            var low = 0;
+            var high = n - 1;
+
+            var e = int.Parse(Console.ReadLine());
+            var startIndex = FindPivot(A, low, high);
+
+            if (A[low] <= e && A[startIndex - 1] >= e)
+            {
+                high = startIndex - 1;
+            }
+            else if (A[startIndex] <= e && A[high] >= e)
+            {
+                low = startIndex;
+            }
+
+            var result = FindElement(A, e, low, high);
+
+            if (result >= 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
         }
     }
 }
