4b96775d3710fd006f5022fcbd027c31fce20ba2
Vincent Gim <vgim@sateva.com.au>
2017-12-16 00:31:02 +0800

    ctci: chp4 - q5 solution using inorder search

---
+++ ctci-practice/chp4-trees-graphs/q5/q5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace q5
+{
+    class Program
+    {
+        public class Node
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int Value { get; set; }
+        }
+
+        private static int previousVisited { get; set; }
+
+        public static bool InorderCheckIsBst(Node node)
+        {
+            if (node == null) { return true; }
+
+            var result = InorderCheckIsBst(node.Left);
+            result = result && (node.Value >= previousVisited);
+            previousVisited = node.Value;
+            result = result && InorderCheckIsBst(node.Right);
+            return result;
+        }
+
+        static Node GetBinaryTree()
+        {
+            var root = new Node
+            {
+                Value = 50,
+                Left = new Node
+                {
+                    Value = 10,
+                    Left = new Node
+                    {
+                        Value = 10
+                    },
+                    Right = new Node
+                    {
+                        Value = 25
+                    }
+                },
+                Right = new Node
+                {
+                    Value = 100,
+                    Left = new Node
+                    {
+                        Value = 75,
+                        Left = new Node
+                        {
+                            Value = 62
+                        },
+                        Right = new Node
+                        {
+                            Value = 80
+                        }
+                    }
+                }
+            };
+            return root;
+        }
+
+        static void Main(string[] args)
+        {
+            var btRoot = GetBinaryTree();
+            var isBst = InorderCheckIsBst(btRoot);
+
+            Console.WriteLine(isBst);
+        }
+    }
+}
