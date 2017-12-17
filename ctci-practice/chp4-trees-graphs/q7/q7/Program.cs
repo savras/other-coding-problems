85f7dd417891aa054e38f7ff7554f85bd6d7691a
Vincent Gim <vgim@sateva.com.au>
2017-12-17 11:11:22 +0800

    ctci: q7 - completed recursive toposort

--- ctci-practice/chp4-trees-graphs/q7/q7/Program.cs
+++ ctci-practice/chp4-trees-graphs/q7/q7/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace q7
+﻿using System;
+using System.Collections.Generic;
+
+namespace q7
 {
     class Program
     {
+        static List<List<int>> GetGraph()
+        {
+            return new List<List<int>>
+            {
+                new List<int>{ 3, 5 },
+                new List<int>{ 5, 3 },
+                new List<int>{ 4 },
+                new List<int>{ 2 },
+                new List<int>(),
+                new List<int>()
+            };
+        }
+
+        static void DFS(List<List<int>> graph, int current, HashSet<int> visited)
+        {
+            foreach (var neighbour in graph[current])
+            {
+                if (!visited.Contains(neighbour))
+                {
+                    visited.Add(neighbour);
+                    DFS(graph, neighbour, visited);
+                }
+            }
+            Console.Write($"{current} ");
+        }
+
+        static void Toposort(List<List<int>> graph)
+        {
+            var visited = new HashSet<int>();
+
+            for(var i = 0; i < graph.Count; i++)
+            {
+                if (!visited.Contains(i))
+                {
+                    visited.Add(i);
+                    DFS(graph, i, visited);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            var graph = GetGraph();
+            Toposort(graph);
         }
     }
 }
