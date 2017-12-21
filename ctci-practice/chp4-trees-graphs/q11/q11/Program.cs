51a230bf300f064e065c157ea86a7459b187861b
Vincent Gim <vgim@sateva.com.au>
2017-12-22 00:43:58 +0800

    ctci: chp4 - q11 initial outline

--- ctci-practice/chp4-trees-graphs/q11/q11/Program.cs
+++ ctci-practice/chp4-trees-graphs/q11/q11/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace q11
+﻿using System;
+
+namespace q11
 {
     class Program
     {
+        static int[] GetT1()
+        {
+            return new[] {0, 1, 2, 3, 4, -1, -1, -1, -1, -1, 5, -1, -1, -1, -1};
+        }
+
+        static int[] GetT2()
+        {
+            return new[] {-1, -1, -1, -1, 4, -1, -1, -1, -1, -1, 5, -1, -1, -1, -1};
+        }
+
+        static bool IsT2SubtreeOfT1(int[] t1, int[] t2)
+        {
+            return false;
+        }
+
         static void Main(string[] args)
         {
+            var t1 = GetT1();
+            var t2 = GetT2();
+
+            var result = IsT2SubtreeOfT1(t1, t2);
+
+            Console.WriteLine(result);
         }
     }
 }
