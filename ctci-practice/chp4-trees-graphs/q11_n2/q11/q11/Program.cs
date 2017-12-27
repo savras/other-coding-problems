50786b1a22b180d86f74cbc582e0e57a90e30a31
Vincent Gim <vgim@sateva.com.au>
2017-12-27 14:44:04 +0800

    ctci: chp4 - q11 updated comments & var names

--- ctci-practice/chp4-trees-graphs/q11_n2/q11/q11/Program.cs
+++ ctci-practice/chp4-trees-graphs/q11_n2/q11/q11/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+ *  This is a BST not a Binary Tree as stated in the question.
+ */ 
+using System;
 
 namespace q11
 {
@@ -9,18 +12,19 @@ namespace q11
             return new[] { 10, 5, 20, 2, 7, 15, 25 };
         }
 
-        static int GetRandomNode(int size)
+        static int GetRandomIndex(int size)
         {
             var rand = new Random();
             return rand.Next(0, size - 1);
         }
+
         static void Main(string[] args)
         {
             var bst = GetBst();
             var size = bst.Length;
 
-            var randomNode = GetRandomNode(size);
-            Console.WriteLine(bst[randomNode]);
+            var randomIndex = GetRandomIndex(size);
+            Console.WriteLine(bst[randomIndex]);
         }
     }
 }
