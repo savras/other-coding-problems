2e405a97a27133ed8d77a27a7eb01a51d4b29826
Vincent Gim <vgim@sateva.com.au>
2017-12-27 14:41:28 +0800

    ctci: chp4 - q11 initial solution

---
+++ ctci-practice/chp4-trees-graphs/q11_n2/q11/q11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace q11
+{
+    class Program
+    {
+        public static int[] GetBst()
+        {
+            return new[] { 10, 5, 20, 2, 7, 15, 25 };
+        }
+
+        static int GetRandomNode(int size)
+        {
+            var rand = new Random();
+            return rand.Next(0, size - 1);
+        }
+        static void Main(string[] args)
+        {
+            var bst = GetBst();
+            var size = bst.Length;
+
+            var randomNode = GetRandomNode(size);
+            Console.WriteLine(bst[randomNode]);
+        }
+    }
+}
