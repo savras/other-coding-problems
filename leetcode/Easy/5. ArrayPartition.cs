66373b401b38d384131f0bdb7ccce08f5defe601
Vincent Gim <vgim@sateva.com.au>
2018-01-04 18:10:42 +0800

    leetcode: q5 solution

---
+++ leetcode/Easy/5. ArrayPartition.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int ArrayPairSum(int[] nums) {
+        Array.Sort(nums);
+        
+        var sum = 0;
+        for(var i = 0; i < nums.Length; i += 2) {
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+}
