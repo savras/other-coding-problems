0b2c302b5bfcc8f44d258488abd158b3169361c6
Vincent Gim <vgim@sateva.com.au>
2018-01-05 17:51:31 +0800

    ctci: chp1 - q2 solution

---
+++ ctci-practice/chp1-arrays/q2/q2/q2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Assumption : case insensitive. Alphabets only.
+
+using System;
+
+namespace q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "mahewiwivhee";
+            var str2 = "eevihwiwehma";
+            var result = IsPermutation(str1, str2);
+            Console.WriteLine(result);
+        }
+
+        static bool IsPermutation(string str1, string str2)
+        {
+            str1 = str1.ToLower();
+            str2 = str2.ToLower();
+
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            var arr = new int[26];
+            for (var i = 0; i < str1.Length; i++)
+            {
+                var index = str1[i] - 'a';
+                arr[index]++;
+            }
+
+            for (var i = 0; i < str2.Length; i++)
+            {
+                var index = str2[i] - 'a';
+                arr[index]--;
+            }
+
+            var result = true;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    result = false;
+                    break;
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
