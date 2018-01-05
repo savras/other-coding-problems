517c97d5ee7e69cb5f6374b60f7ec6975195a25c
Vincent Gim <vgim@sateva.com.au>
2018-01-05 23:46:37 +0800

    ctci: chp1 q4 - solution

---
+++ ctci-practice/chp1-arrays/q6/q6/q6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var str = "aaabbbCCCdddEEEEEEEEa";
+            var str = "abc";
+            var result = GetCompressedString(str);
+            Console.WriteLine(result);
+        }
+
+        static string GetCompressedString(string str)
+        {
+            str = str + "-";    // Trigger change at the end of the string
+            var lastIndex = 0;
+            var lastChar = str[lastIndex];
+            var strLen = str.Length;
+
+            var sb = new StringBuilder();
+            for (var i = 1; i < strLen; i++)
+            {
+                if (str[i] != lastChar)
+                {
+                    sb.Append(lastChar);
+                    sb.Append(i - lastIndex);
+                    lastIndex = i;
+                    lastChar = str[i];
+                }
+
+                // Potentially check length of sb against strLen;
+            }
+
+            if (strLen - 1 < sb.Length)
+            {
+                return str.Substring(0, strLen - 1);
+            }
+            return sb.ToString();
+        }
+    }
+}
