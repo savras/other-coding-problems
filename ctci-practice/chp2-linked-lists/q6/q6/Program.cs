abb7e185422fe83584ec6a7bfdc07091be168659
Vincent Gim <vgim@sateva.com.au>
2018-01-09 01:17:38 +0800

    ctci: chp2 - q6 initial solution

---
+++ ctci-practice/chp2-linked-lists/q6/q6/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace q6
+{
+    class Program
+    {
+        public class Node
+        {
+            public Node(string val)
+            {
+                Value = val;
+            }
+            public Node Next;
+            public string Value;
+        }
+        static void Main(string[] args)
+        {
+
+            var list = new Node("a");
+            list.Next = new Node("b");
+            list.Next.Next = new Node("c");
+            list.Next.Next.Next = new Node("d");
+            list.Next.Next.Next.Next = new Node("c");
+            list.Next.Next.Next.Next.Next = new Node("b");
+            list.Next.Next.Next.Next.Next.Next = new Node("a");
+
+            var result = IsPalindrome(list);
+
+        }
+
+        static bool IsPalindrome(Node node)
+        {
+
+            return false;
+        }
+    }
+}
