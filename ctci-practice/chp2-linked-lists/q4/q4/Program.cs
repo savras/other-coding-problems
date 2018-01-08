c66a142ecc0f1a018760e1a84b7f56d0110f51c9
Vincent Gim <vgim@sateva.com.au>
2018-01-09 01:09:02 +0800

    ctci: chp2 - q4 solution

---
+++ ctci-practice/chp2-linked-lists/q4/q4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace q4
+{
+    class Program
+    {
+        class Node
+        {
+            public Node(int val)
+            {
+                Value = val;
+            }
+            public Node Next;
+            public int Value;
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new Node(3);
+            list.Next = new Node(5);
+            list.Next.Next = new Node(8);
+            list.Next.Next.Next = new Node(5);
+            list.Next.Next.Next.Next = new Node(10);
+            list.Next.Next.Next.Next.Next = new Node(2);
+            list.Next.Next.Next.Next.Next.Next = new Node(1);
+
+            var val = 5;
+            var head = Partition(list, val);
+
+            var current = head;
+            while (current != null)
+            {
+                Console.Write($"{current.Value} ");
+                current = current.Next;
+            }
+        }
+
+        static Node Partition(Node node, int val)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+
+            var head = node;
+            var prev = node;
+            var current = node.Next;
+
+            while (current != null)
+            {
+                if (current.Value < val)
+                {
+                    prev.Next = current.Next;
+                    current.Next = head;
+                    head = current;
+                    current = prev.Next;
+                }
+                else
+                {
+                    prev = current;
+                    current = current.Next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
