ada801304e3e90a50221675c484687b06885d3dd
Vincent Gim <vgim@sateva.com.au>
2018-01-09 02:35:44 +0800

    ctci: chp2 - q6: completed question

--- ctci-practice/chp2-linked-lists/q6/q6/Program.cs
+++ ctci-practice/chp2-linked-lists/q6/q6/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace q6
+﻿using System;
+using System.Collections.Generic;
+
+namespace q6
 {
     class Program
     {
+        public static int Current = -1; // zero based
+
         public class Node
         {
             public Node(string val)
@@ -18,18 +23,34 @@
             list.Next = new Node("b");
             list.Next.Next = new Node("c");
             list.Next.Next.Next = new Node("d");
-            list.Next.Next.Next.Next = new Node("c");
-            list.Next.Next.Next.Next.Next = new Node("b");
-            list.Next.Next.Next.Next.Next.Next = new Node("a");
+            list.Next.Next.Next.Next = new Node("b");
+            list.Next.Next.Next.Next.Next = new Node("a");
 
-            var result = IsPalindrome(list);
-
+            var size = 6;
+            var result = IsPalindrome(list, size, new Stack<string>());
+            Console.WriteLine(result);
         }
 
-        static bool IsPalindrome(Node node)
+        static bool IsPalindrome(Node node, int size, Stack<string> s)
         {
+            Current++;
+            if (node == null || Current >= size)
+            {
+                return true;
+            }
 
-            return false;
+            if (size%2 != 0 && Current == size/2)
+            {
+                // Is middle value for odd no. size
+                return IsPalindrome(node.Next, size, s);
+            }
+
+            if (Current < (size/2))
+            {
+                s.Push(node.Value);
+                return IsPalindrome(node.Next, size, s);
+            }
+            return s.Pop() == node.Value && IsPalindrome(node.Next, size, s);
         }
     }
 }
