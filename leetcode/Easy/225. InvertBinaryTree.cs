cd6e62e931cb3772545b0cf7d7239ddf770adb5c
Vincent Gim <vgim@sateva.com.au>
2018-01-14 12:31:32 +0800

    leetcode: q225 solution

---
+++ leetcode/Easy/225. InvertBinaryTree.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }        
+        
+        var temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        InvertTree(root.left);
+        InvertTree(root.right);
+        
+        return root;
+    }
+}
