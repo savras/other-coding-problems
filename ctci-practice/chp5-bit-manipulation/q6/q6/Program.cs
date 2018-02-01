b7b5e8fed5a6faefc6d122d66f37def97cf708d6
Vincent Gim <vgim@sateva.com.au>
2018-02-02 01:14:02 +0800

    ctci: chp5 - q6 solution

---
+++ ctci-practice/chp5-bit-manipulation/q6/q6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace q6
+{
+    class Program
+    {
+        static int GetNumberOfOnesToFlip(int value1, int value2)
+        {
+            var xor = value1 ^ value2;
+
+            var result = 0;
+            while (xor > 0)
+            {
+                if ((xor & 1) == 1)
+                {
+                    result++;
+                }
+                xor = xor >> 1;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var value1 = 23423;
+            var value2 = 7654;
+            var result = GetNumberOfOnesToFlip(value1, value2);
+            Console.WriteLine(result);
+        }
+    }
+}
