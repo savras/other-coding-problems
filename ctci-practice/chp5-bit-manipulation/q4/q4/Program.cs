e11aa8b81e40a2d7291d965661fdfad6c889bd89
Vincent Gim <vgim@sateva.com.au>
2018-02-02 01:04:15 +0800

    ctci: chp5 - q4 incorrect solution

---
+++ ctci-practice/chp5-bit-manipulation/q4/q4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = 1778;
+            var nextLargestResult = GetNextLargestBinaryWithSameOneCount(value);
+            var nextSmallestResult = GetNextSmallestBinaryWithSameOneCount(value);
+        }
+
+        private static int GetNextLargestBinaryWithSameOneCount(int value)
+        {
+            var extractedLsb = 1;
+            var count = 0;
+
+            while (extractedLsb != 0)
+            {
+                extractedLsb = value & 1;
+                if (extractedLsb == 1)
+                {
+                    value = value << 1;
+                    count++;
+
+                }
+            }
+
+            value++;
+            value = value << count;
+
+            var replaceOnes = 0;    // Replace the number of 1's that we have removed.
+            if (count > 0)
+            {
+                replaceOnes = count - 1;    // We remove exactly one 1 from value, so we exclude that from count
+            }
+            return value + (count - replaceOnes);
+        }
+
+        private static object GetNextSmallestBinaryWithSameOneCount(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
