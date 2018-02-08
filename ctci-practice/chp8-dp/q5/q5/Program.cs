31bf5df006ea78651018cb7f7f96449bccdf17f4
Vincent Gim <vgim@sateva.com.au>
2018-02-08 23:32:40 +0800

    ctci: chp8 - q5 initial solution

---
+++ ctci-practice/chp8-dp/q5/q5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace q5
+{
+    class Program
+    {
+        static int Multiply(int val, int count)
+        {
+            if (count == 0)
+            {
+                return 0;
+            }
+            return Multiply(val, count - 1) + val;
+        }
+
+        static void Main(string[] args)
+        {
+            var result = Multiply(4, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
