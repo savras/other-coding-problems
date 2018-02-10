64a8c9b88235f656bd718a453884e7ab7f2e26af
Vincent Gim <vgim@sateva.com.au>
2018-02-10 23:04:40 +0800

    ctci: chp8 - q5 initial recursive solution

--- ctci-practice/chp8-dp/q5/q5/Program.cs
+++ ctci-practice/chp8-dp/q5/q5/Program.cs
@@ -4,18 +4,47 @@ namespace q5
 {
     class Program
     {
-        static int Multiply(int val, int count)
+        // Primitive method
+        // Doesn't handle negative numbers.
+        static int Primitive(int val, int count)
         {
             if (count == 0)
             {
                 return 0;
             }
-            return Multiply(val, count - 1) + val;
+            
+            return CallMultiplyWithSmallerAsFirstArgument(val, count - 1) + val;
+        }
+
+        // More optimal method
+        static int CallMultiplyWithSmallerAsFirstArgument(int a, int b)
+        {
+            var smaller = a;
+            var larger = b;
+            if (a > b)
+            {
+                smaller = b;
+                larger = a;
+            }
+            return Multiply(smaller, larger);
+        }
+
+        static int Multiply(int smaller, int larger)
+        {
+            if (smaller == 0) { return 0; }
+            if (smaller == 1) { return larger; }
+
+            var newSmaller = smaller >> 1;  // Half the value
+            return Multiply(newSmaller, larger) << 1;
         }
 
         static void Main(string[] args)
         {
-            var result = Multiply(4, 3);
+            var a = 4;
+            var b = 8;
+
+            var result = Multiply(a, b);
+            
             Console.WriteLine(result);
         }
     }
