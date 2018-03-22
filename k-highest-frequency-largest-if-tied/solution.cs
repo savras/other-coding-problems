66769e86ecb69797b39033dc237df5bf25466413
Savras <d20.savras@gmail.com>
2018-03-22 21:40:26 +0800

    k highest frequency num in unsorted array

---
+++ k-highest-frequency-largest-if-tied/solution.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Solution
+    {
+        // Find the Kth most occurring number in an unsorted array.
+        // If their frequency are tied, output the largest number among them
+        public static void KthOccurrence(int[] arr, int k)
+        {
+            var dict = new Dictionary<int, int>();  // Value is the frequency of occurrence.
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (!dict.ContainsKey(arr[i]))
+                {
+                    dict.Add(arr[i], 1);
+                }
+                else
+                {
+                    dict[arr[i]]++;
+                }
+            }
+
+            var sortedDict = dict.OrderByDescending(d => d.Value)
+                .ThenByDescending(d => d.Key).ToList();
+
+            for (var i = 0; i < k; i++)
+            {
+                Console.WriteLine(sortedDict[i].Key);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            KthOccurrence(new[] {7, 3, 2, 4, 7, 9, 3, 2, 3, 3, 9, 7, 5, 9}, 3);
+        }
+    } 
+}
