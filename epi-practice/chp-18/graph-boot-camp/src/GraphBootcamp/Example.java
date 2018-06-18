2c3d2211e5ecd158025222df42e94b0566d71de3
Savras <d20.savras@gmail.com>
2018-06-18 22:16:08 +0800

    Added comments to boot-camp solution

--- epi-practice/chp-18/graph-boot-camp/src/GraphBootcamp/Example.java
+++ epi-practice/chp-18/graph-boot-camp/src/GraphBootcamp/Example.java
@@ -5,10 +5,9 @@ import java.util.*;
 public class Example {
     public static void main(String[] arguments) {
         List<MatchResult> mrs = new ArrayList<>();
-        mrs.add(new MatchResult("a", "b"));
-        mrs.add(new MatchResult("a", "b"));
-        mrs.add(new MatchResult("b", "a"));
-        MatchResult mr = new MatchResult("a", "b");
+        mrs.add(new MatchResult("a", "c"));
+        mrs.add(new MatchResult("c", "d"));
+        mrs.add(new MatchResult("d", "b"));
 
         System.out.println(canTeamABeatTeamB(mrs, "a", "b"));
     }
@@ -28,10 +27,14 @@ public class Example {
         return isReachableDFS(buildGraph(matches), teamA, teamB, visited);
     }
 
+    /**
+     * Build a dictionary where the key is a winning team and the value is a hash set of teams that
+     * the key has beaten.
+     */
     private static Map<String, Set<String>> buildGraph(List<MatchResult> matches) {
         Map<String, Set<String>> graph = new HashMap<>();
-        Set<String> edges = new HashSet<>();
         for (MatchResult match : matches) {
+            Set<String> edges = graph.get(match.winningTeam);
             if (edges == null) {
                 edges = new HashSet<>();
                 graph.put(match.winningTeam, edges);
@@ -42,11 +45,11 @@ public class Example {
     }
 
     public static boolean isReachableDFS(Map<String, Set<String>> graph, String curr, String dest, Set<String> visited) {
-        if (curr.equals(dest)) {
-            return true;
-        } else if (visited.contains(curr) || graph.get(curr) == null) {
-            return false;
-        }
+            if (curr.equals(dest)) {
+                return true;
+            } else if (visited.contains(curr) || graph.get(curr) == null) {
+                return false;
+            }
 
         visited.add(curr);
         for (String team : graph.get(curr)) {
