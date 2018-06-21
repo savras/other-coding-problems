d2b9d7b7c9eb1a1de02b34806b06de8bb57fa8d1
Savras <d20.savras@gmail.com>
2018-06-22 00:51:45 +0800

    Push misc commits

---
+++ ctci-practice/chp8-dp/q14/q14/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace q14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ maximum-i-j-arri-arrj/solution/solution/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] {9, 5, 7, 10, 3, 18, 1, 23};
+
+            var len = arr.Length;
+            var value = arr[0];
+            for (var i = 1; i < arr.Length; i++)
+            {
+                var distSoFar = len - i;
+
+            }
+        }
+    }
+}
